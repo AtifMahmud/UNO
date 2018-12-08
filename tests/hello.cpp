03873c4b7a892c37c795cf7739d14cdee97e8182
atifmahmud <atifmahmud95@gmail.com>
2018-12-08 00:51:06 -0800

    Added test hello-world

---
+++ tests/hello.cpp
@@ -0,0 +1,14 @@
+/**
+ * A file to test if C++ is compiling and running on my machine
+ *
+ */
+
+ #include <iostream>
+ using namespace std;
+
+ int main () 
+ {
+    cout << "Hello, World!\n";
+
+    return 0;
+ }
